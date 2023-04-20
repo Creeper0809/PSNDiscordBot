139f9f3302ec81b62e6ff76c74af73ead1159844
Creeper0809 <creeper0809@hotmail.com>
2023-04-20 16:37:52 +0900

    d

---
+++ Baekjoon.py
@@ -0,0 +1,69 @@
+import discord
+from discord.ext import commands
+import json
+import requests
+
+def get_profile(user_id):
+    url = "https://solved.ac/api/v3/user/show"
+    params = {
+        "handle": user_id
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    response = requests.get(url, params=params, headers=headers)
+
+    if response.status_code == requests.codes.ok:
+        return json.loads(response.text)
+    else:
+        return None
+def setup(app):
+    @app.command(aliases=['안녕', 'hi', '안녕하세요'])
+    async def ping(ctx):
+        await ctx.channel.send(f'{ctx.author.mention}님 안녕하세요!')
+
+    @app.command(aliases=['백준'])
+    async def roll(ctx, user_id: str):
+        profile = get_profile(user_id)
+        tier = {
+            1: 'Bronze V',
+            2: 'Bronze IV',
+            3: 'Bronze III',
+            4: 'Bronze II',
+            5: 'Bronze I',
+            6: 'Silver V',
+            7: 'Silver IV',
+            8: 'Silver III',
+            9: 'Silver II',
+            10: 'Silver I',
+            11: 'Gold V',
+            12: 'Gold IV',
+            13: 'Gold III',
+            14: 'Gold II',
+            15: 'Gold I',
+            16: 'Platinum V',
+            17: 'Platinum IV',
+            18: 'Platinum III',
+            19: 'Platinum II',
+            20: 'Platinum I',
+            21: 'Diamond V',
+            22: 'Diamond IV',
+            23: 'Diamond III',
+            24: 'Diamond II',
+            25: 'Diamond I',
+            26: 'Ruby V',
+            27: 'Ruby IV',
+            28: 'Ruby III',
+            29: 'Ruby II',
+            30: 'Ruby I',
+            31: 'Master'
+        }
+        if profile is not None:
+            message = f"유저 {user_id}님의 프로필 정보:\n"
+            message += f"티어: {tier[profile['tier']]}\n"
+            message += f"Ranking: {profile['rank']}\n"
+            message += f"Rating: {profile['rating']}\n"
+            message += f"exp: {profile['exp']}\n"
+            await ctx.send(message)
+        else:
+            await ctx.send("프로필 요청 실패")
---
+++ main.py
@@ -0,0 +1,76 @@
+import asyncio
+import random
+
+import Baekjoon
+import Database
+import discord
+from discord.ext import commands
+
+import RockSissorPaper
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.guilds = True
+intents.members = True
+app = commands.Bot(command_prefix='/', intents=intents)
+
+RockSissorPaper.setup(app)
+Baekjoon.setup(app)
+
+PREFIX = '/'
+
+database = Database.database()
+
+@app.event
+async def on_ready():
+    print('Done')
+    await app.change_presence(status=discord.Status.online, activity=None)
+
+@app.command()
+async def 회원가입(ctx):
+    if database.add_user(ctx.message.author):
+        await ctx.channel.send(f"{ctx.message.author.mention}님 회원가입을 추가합니다!")
+    else:
+        await ctx.channel.send("이미 가입 된 회원입니다")
+
+@app.command()
+async def 유저보기(ctx,user : discord.User):
+    info = database.find_user(user.id)
+    if info is None:
+        await ctx.channel.send(f"{user.mention}은 없는 회원입니다")
+    await ctx.channel.send(f"{user.mention}은 {info.point}원을 가지고 계십니다.")
+
+@app.command()
+async def 돈추가(ctx,message):
+    info = database.find_user(ctx.message.author.id)
+    if info is None:
+        await ctx.channel.send(f"{ctx.message.author.mention}은 회원가입부터 해주십시오")
+    if not str.isdigit(message):
+        return
+    info.point += int(message)
+    await ctx.channel.send(f"{ctx.message.author.mention}은 {info.point}원을 가지고 계십니다.")
+
+@app.command()
+async def 가위바위보(ctx,message):
+    alias = ["가위", "바위", "보"]
+    computer = alias[random.randint(0, len(alias) - 1)]
+    player = message
+    await ctx.channel.send("컴퓨터는 " + computer + "를 냈다")
+    if (player == "가위" and computer == "보") or (player == "바위" and computer == "가위") or (
+            player == "보" and computer == "바위"):
+        result = "플레이어가 이겼다!"
+    elif (player == "가위" and computer == "가위") or (player == "바위" and computer == "바위") or (
+            player == "보" and computer == "보"):
+        result = "플레이어와 컴퓨터가 비겼다"
+    else:
+        result = "플레이어가 졌다!"
+    await ctx.channel.send(result)
+
+
+@app.command()
+async def 하이(ctx):
+    print()
+    await ctx.send('Hello I am Bot!')
+
+if __name__=="__main__":
+    app.run('')
