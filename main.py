fec999c463c1b5c50437f37a7b64a08179c70763
Creeper0809 <creeper0809@hotmail.com>
2023-04-21 15:07:15 +0900

    d

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import os
+
+import pymysql
+
 import Baekjoon
 import CommonCommand
-import Database
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -10,9 +14,18 @@ intents.guilds = True
 intents.members = True
 PREFIX = '/'
 app = commands.Bot(command_prefix=PREFIX, intents=intents)
-database = Database.database()
-Baekjoon.setup(app)
-CommonCommand.setup(app,database)
+load_dotenv()
+
+mysql = pymysql.connect(
+    host=os.environ.get("HOST"),
+    port=os.environ.get("PORT"),
+    user=os.environ.get("USER"),
+    password=os.environ.get("PASSWORD"),
+    database=os.environ.get("DATABASE")
+)
+
+Baekjoon.setup(app,mysql)
+CommonCommand.setup(app,mysql)
 
 
 @app.event
@@ -21,4 +34,4 @@ async def on_ready():
     await app.change_presence(status=discord.Status.online, activity=None)
 
 if __name__=="__main__":
-    app.run('')
+    app.run(os.environ.get("TOKEN"))
